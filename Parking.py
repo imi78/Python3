86fb412e8011e70cf58cdf61a17b06328cdab083
imi78 <Imi78@abv.bg>
2020-11-16 10:29:10 +0000

    added total sum

--- Parking.py
+++ Parking.py
@@ -1,8 +1,9 @@
 days = int(input())
 hours = int(input())
-price = 0
+total = 0
 
 for day in range(1, days + 1):
+  price = 0
   for hour in range(1, hours + 1):
     if day % 2 == 0 and hour % 2 == 1:
       price += 2.50
@@ -10,4 +11,6 @@ for day in range(1, days + 1):
       price += 1.25
     else: 
       price += 1
-print (price:.2f)
+  print (f'Day: {day} - {price}')
+  total +=price
+print(total)
