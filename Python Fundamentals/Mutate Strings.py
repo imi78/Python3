9d9c239467cd8c039a82c9146003ec93b487e753
Ivelin <imi78@abv.bg>
2020-11-29 19:58:19 +0200

    project completed

--- Python Fundamentals/Mutate Strings.py
+++ Python Fundamentals/Mutate Strings.py
@@ -1,13 +1,16 @@
 str_1 = input()
 str_2 = input()
-str_3 = ''
-counter = 1
+current_string = ''
+previous_string = str_1
 
 for i in range(len(str_1)):
-    for j in range(len(str_2)):
-        str_3 = str_2[i:counter] + str_1[j+1:]
-        if counter == len(str_1):
-            break
-        else:
-            counter += 1
-            print(str_3)
+    for index_str_2 in range(0, i+1):
+        current_string += str_2[index_str_2]
+    for index_str_1 in range(i + 1, len(str_1)):
+        current_string += str_1[index_str_1]
+
+    if not previous_string == current_string:
+        print(current_string)
+
+    previous_string = current_string
+    current_string = ''
--- main.py
+++ main.py
@@ -2,11 +2,11 @@ str_1 = input()
 str_2 = input()
 str_3 = ''
 counter = 1
-
+str_4 = str_1
 for i in range(len(str_1)):
     for j in range(len(str_2)):
         str_3 = str_2[i:counter] + str_1[j+1:]
-        if counter == len(str_1):
+        if str_3 == str_4:
             break
         else:
             counter += 1
