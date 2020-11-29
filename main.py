a68c3f63e05340303f5cd705209a39f7057aab10
Ivelin <imi78@abv.bg>
2020-11-29 21:15:23 +0200

    project completed

--- main.py
+++ main.py
@@ -1,15 +1,8 @@
-str_1 = input()
-str_2 = input()
-str_3 = ''
-counter = 1
-str_4 = str_1
-for i in range(len(str_1)):
-    for j in range(len(str_2)):
-        str_3 = str_2[i:counter] + str_1[j+1:]
-        if str_3 == str_4:
-            break
-        else:
-            counter += 1
-            print(str_3)
+divisor = int(input())
+bound = int(input())
+
+res = int(bound/divisor)
+res_1 = res * divisor
+# print(res)
 
 
