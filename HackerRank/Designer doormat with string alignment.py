6c7be8cc9a5c3cefcff46c77fd5dae0eda842307
Ivelin <imi78@abv.bg>
2021-03-12 13:58:28 +0200

    comments

--- HackerRank/Designer doormat with string alignment.py
+++ HackerRank/Designer doormat with string alignment.py
@@ -4,10 +4,10 @@ pattern = '.|.'
 # print((pattern).center(m, '-'))
 # print((pattern*3).center(m, '-'))
 
-for i in range(0, n-1, 2):
+for i in range(0, n-1, 2):  # first rows with the pattern
     print((pattern*(i+1)).center(m, '-'))
 
-print('WELCOME'.center(m, '-'))
+print('WELCOME'.center(m, '-'))  # prints in the middle
 
-for i in range(n, 1, -2):
+for i in range(n, 1, -2):  # last rows with pattern
     print((pattern*(i-2)).center(m, '-'))
