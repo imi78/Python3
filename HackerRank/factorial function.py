56f09315e645aaca2f0f4cb70feb32ef6ba07e5a
imi78 <>
2021-04-05 10:48:39 +0000

    not completely working

---
+++ HackerRank/factorial function.py
@@ -0,0 +1,12 @@
+
+def Factorial(n):
+    if n <= 1:
+        return 1
+    else:
+        sum = n * Factorial(n-1)
+        return sum
+
+
+
+
+print(Factorial(1))
--- main.py
+++ main.py
@@ -1,8 +1,8 @@
-dict = {'orange': 'портокал', 'car': 'кола'}
-# second = {'cat': 'котка', 'dog': 'куче'}
-for k, v in {'cat': 'котка', 'dog': 'куче'}.items():
-  dict.update({k:v})
+# dict = {'orange': 'портокал', 'car': 'кола'}
+# # second = {'cat': 'котка', 'dog': 'куче'}
+# for k, v in {'cat': 'котка', 'dog': 'куче'}.items():
+#   dict.update({k:v})
 
 
-print(dict['cat'], dict['orange'])
-print(dict['dog'])
+# print(dict['cat'], dict['orange'])
+# print(dict['dog'])
