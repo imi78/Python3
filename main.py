1b15a1c828856d66aba327992c401483c18c3798
imi78 <>
2021-04-07 13:10:24 +0000

    itertools playground

--- main.py
+++ main.py
@@ -6,3 +6,13 @@
 
 # print(dict['cat'], dict['orange'])
 # print(dict['dog'])
+
+from itertools import product
+
+
+A = list(map(int, (input()).split(' ')))
+B = list(map(int, (input()).split(' ')))
+
+a = list(product((A), (B)))
+for i in a:
+    print (i, end=" ")
