faf6a739323b4b92d7414392ac31fe8dccd202dd
imi78 <>
2021-05-10 16:38:57 +0000

    ...

--- main.py
+++ main.py
@@ -1,8 +1,21 @@
-# dict = {'orange': 'портокал', 'car': 'кола'}
-# # second = {'cat': 'котка', 'dog': 'куче'}
-# for k, v in {'cat': 'котка', 'dog': 'куче'}.items():
-#   dict.update({k:v})
+import requests
+url = 'http://data.fixer.io/api/latest?access_key=c1bf0b4af0dd8939a2da6c80e10c5bbf'
+data = requests.get(url).json()
+rates = {}
 
 
-# print(dict['cat'], dict['orange'])
-# print(dict['dog'])
+def convert(from_currency, to_currency, amount):   
+    initial_amount = amount
+    rates = data['rates']
+    if from_currency != 'EUR':
+        amount = amount / rates[from_currency]  # calculates the base currency (EUR) rate.
+
+    amount = round(amount * rates[to_currency], 2)  # then re-calculates the rate to the given currency
+    print(f'{initial_amount} {from_currency} = {amount:.2f} {to_currency}')
+
+from_currency = input("From currency: ").upper()
+to_currency = input("To currency: ").upper()
+amount = int(input("Amount: "))
+
+   
+convert(from_currency, to_currency, amount)
