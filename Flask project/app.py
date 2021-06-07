c11104cc4ba1432c875a10f233e09caf30ba1f1d
Ivelin <imi78@abv.bg>
2021-06-07 20:56:38 +0300

    polished files

--- Flask project/app.py
+++ Flask project/app.py
@@ -2,13 +2,19 @@ import requests
 from flask import Flask, render_template, request
 app = Flask(__name__)
 
-
-@app.route('/', methods=['GET', 'POST'])
+@app.route('/')
 def index():
     url = 'http://data.fixer.io/api/latest?access_key=c1bf0b4af0dd8939a2da6c80e10c5bbf'
     data = requests.get(url).json()
     rates = data['rates']
     return render_template("index.html", rates=rates)
-    
-@app.route('/', methods =["GET", "POST"])
 
+@app.route('/', methods=['GET', 'POST'])
+def convert():
+    url = 'http://data.fixer.io/api/latest?access_key=c1bf0b4af0dd8939a2da6c80e10c5bbf'
+    data = requests.get(url).json()
+    rates = data['rates']
+    from_currency = request.form["from_currency"]
+    to_currency = request.form["to_currency"]
+    amount = request.form["amount"]
+    return render_template("index.html", rates=rates, from_currency=from_currency, to_currency=to_currency, amount=amount)
