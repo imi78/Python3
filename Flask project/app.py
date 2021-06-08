ba38ef48efe8462e68347d8fc4aab9164f94934c
Ivelin <imi78@abv.bg>
2021-06-08 20:24:43 +0300

    working Flask app

--- Flask project/app.py
+++ Flask project/app.py
@@ -2,18 +2,21 @@ import requests
 from flask import Flask, render_template, request
 app = Flask(__name__)
 
-url = 'http://data.fixer.io/api/latest?access_key=c1bf0b4af0dd8939a2da6c80e10c5bbf'
-data = requests.get(url).json()
-rates = data['rates']
-print(rates)
-
-@app.route('/')
-def index():
-    return render_template("index.html", rates=rates)
+currencies = f"https://free.currconv.com/api/v7/currencies?apiKey=fe444f880c2cb85b3f6a"
+rates = requests.get(currencies).json()['results']
 
 @app.route('/', methods=['GET', 'POST'])
-def convert():
-    from_currency = request.form["from_currency"]
-    to_currency = request.form["to_currency"]
-    amount = request.form["amount"]
-    return render_template("index.html", rates=rates, from_currency=from_currency, to_currency=to_currency, amount=amount)
+def index():
+
+    if request.method == "POST":
+        from_currency = request.form["from_currency"]
+        to_currency = request.form["to_currency"]
+        amount = request.form["amount"]
+        url = f'https://free.currconv.com/api/v7/convert?q={from_currency}_{to_currency}&compact=ultra&apiKey=fe444f880c2cb85b3f6a'
+        rate = requests.get(url).json()[f"{from_currency}_{to_currency}"]
+        amount = int(amount)
+        initial_amount = amount
+        amount = round (amount * rate, 2)
+        return render_template('index.html', rates=rates, result=f'{initial_amount} {from_currency} = {amount} {to_currency}')
+
+    return render_template('index.html', rates=rates)
