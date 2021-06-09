db0ebafe2661d3ce4d40e90f63f49bbf278ec9b3
imi78 <57959690+imi78@users.noreply.github.com>
2021-06-09 15:42:09 +0300

    Update app.py

--- Flask project/app.py
+++ Flask project/app.py
@@ -1,9 +1,15 @@
 import requests
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, flash, url_for, redirect
 app = Flask(__name__)
 
+app.secret_key = 'SECRET_KEY'
+
 currencies = f"https://free.currconv.com/api/v7/currencies?apiKey=fe444f880c2cb85b3f6a"
-rates = requests.get(currencies).json()['results']
+data = requests.get(currencies).json()['results']
+rates = []
+for k, v in data.items():
+  rates.append(f"{v['id']} -  {v['currencyName']}")
+rates.sort()
 
 @app.route('/', methods=['GET', 'POST'])
 def index():
@@ -12,11 +18,15 @@ def index():
         from_currency = request.form["from_currency"]
         to_currency = request.form["to_currency"]
         amount = request.form["amount"]
-        url = f'https://free.currconv.com/api/v7/convert?q={from_currency}_{to_currency}&compact=ultra&apiKey=fe444f880c2cb85b3f6a'
-        rate = requests.get(url).json()[f"{from_currency}_{to_currency}"]
-        amount = int(amount)
-        initial_amount = amount
-        amount = round (amount * rate, 2)
-        return render_template('index.html', rates=rates, result=f'{initial_amount} {from_currency} = {amount} {to_currency}')
+        if from_currency != "From currency" and to_currency != "To currency" and amount!= "":
+            amount = float(amount)
+            initial_amount = amount
+            url = f'https://free.currconv.com/api/v7/convert?q={from_currency}_{to_currency}&compact=ultra&apiKey=fe444f880c2cb85b3f6a'
+            rate = requests.get(url).json()[f"{from_currency}_{to_currency}"]
+            amount = round (amount * rate, 2)
+            return render_template('index.html', rates=rates, result=f'{initial_amount} {from_currency} = {amount} {to_currency}')
+        else:
+            flash("Holy guacamole! You should pick currency.")
+            return redirect(url_for('index'))
 
     return render_template('index.html', rates=rates)
