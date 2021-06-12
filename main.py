a01695e82fc6e37a3b5caba66001c20907660b78
imi78 <57959690+imi78@users.noreply.github.com>
2021-06-12 12:57:24 +0000

    updated icons

--- main.py
+++ main.py
@@ -14,6 +14,9 @@ from kivy.properties import ObjectProperty
 currencies = f"https://free.currconv.com/api/v7/currencies?apiKey=fe444f880c2cb85b3f6a"
 data = requests.get(currencies).json()['results']
 rates = []
+for k, v in data.items():
+  rates.append(f"{v['id']} -  {v['currencyName']}")
+rates.sort()
 
 class Grid(Widget):  # this is accessible by "root." not by "Grid."
     fromCurr = ObjectProperty(None)
@@ -21,18 +24,18 @@ class Grid(Widget):  # this is accessible by "root." not by "Grid."
     amount  = ObjectProperty(None)
 
     def btn(self):  # function for conversion 
-       
-        amount = self.amount.text  # self.amout.text is a <str>
-        if self.fromCurr in rates and self.toCurr in rates and amount.isdigit():
-          initial_amount = int(amount)
-          amount = amount / rates[self.fromCurr]  # calculates the base currency (EUR) rates
-          amount = round(amount * rates[self.toCurr], 2)  # then re-calculates the rate to the given currency
-          print(f'{initial_amount} {self.fromCurr} = {amount:.2f} {self.toCurr}')
-        else:
-          if self.fromCurr not in rates or self.toCurr not in rates:
-            print('Enter a valid three letter currency')
-          if amount.isdigit() == False:
-            print('Enter a valid amount')
+      
+      amount = self.amount.text  # self.amout.text is a <str>
+      if self.fromCurr in rates and self.toCurr in rates and amount.isdigit():
+        initial_amount = int(amount)
+        amount = amount / rates[self.fromCurr]  # calculates the base currency (EUR) rates
+        amount = round(amount * rates[self.toCurr], 2)  # then re-calculates the rate to the given currency
+        print(f'{initial_amount} {self.fromCurr} = {amount:.2f} {self.toCurr}')
+      else:
+        if self.fromCurr not in rates or self.toCurr not in rates:
+          print('Enter a valid three letter currency')
+        if amount.isdigit() == False:
+          print('Enter a valid amount')
         
         # self.fromCurr.text = ""  # cleares the text input after submit button is clicked
         # self.toCurr.text = ""
