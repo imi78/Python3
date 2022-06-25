8c0952dd0a0a93870a8297b7268481e58f1f95e6
imi78 <57959690+imi78@users.noreply.github.com>
2022-06-25 21:10:14 +0300

    Python script for SQL queries

--- SQL/test.py
+++ SQL/test.py
@@ -1,13 +1,25 @@
 import sqlite3
 
+# Connect to the DB
 conn = sqlite3.connect('/home/ivo/Python/Python3/SQL/db.sqlite')
 
+# Cursor
 curr = conn.cursor()
 
-command = "SELECT * FROM Player"
+#List with SQL queries to be executed. Just add input to the list
+commands = [
+'SELECT * FROM Player',
+'SELECT id, player_name as name, birthday, height, weight FROM Player WHERE height>150',
+'SELECT name FROM Country'
+]
 
-curr.execute(command)
+# Execute the commands one by one
+for command in commands:
+    curr.execute(command)
+    res = curr.fetchall() # Fetch all entries
+    for i in res[:10]: # Only first ten will be printed
+        print(i)
+    
+    # adds space between results for readability
+    print() 
 
-res = curr.fetchall()
-for i in res[:10]:
-    print(i)
