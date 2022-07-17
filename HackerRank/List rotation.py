d66532595a9f60dfca4f7dfb4b2991d580a92a0e
imi78 <imi78@abv.bg>
2022-07-17 20:47:43 +0300

    list rotation

--- HackerRank/List rotation.py
+++ HackerRank/List rotation.py
@@ -1,17 +1,20 @@
-lst = [0,1,4,4,5,6,7]
-n = 6
-
-# Rotate the last element to index [0] n times
-for i in range(n):
-  # if lenght of the list is equal to the rotation cycles it shoud return the same list
-    if n == len(lst):
-        print(lst)
-        break
+def list_rotation(lst, n):
+  
+  # Rotate the last element to index [0] n times
+  for i in range(n):
+    
+    # if lenght of the list is equal to the rotation cycles it shoud return the same list
+    if n == len(lst):   
+      return(lst)
+      break
       
       # or cut the last element, add it at index[0] and replace the original list
     else:
-        lst = lst[-1:] + lst[:-1]
-      
-      # checks if the loop has finished to print the list
-        if i == (n-1):
-            print(lst)
+      lst = lst[-1:] + lst[:-1]
+  return lst
+
+if __name__ == '__main__':
+  lst = [0,1,4,4,5,6,7]
+  n = 6
+  print(list_rotation(lst, n))
+  
--- main.py
+++ main.py
@@ -5,7 +5,6 @@ conn = sqlite3.connect('/home/ivo/Python3/SQL/db.sqlite')
 curr = conn.cursor()
 curr.execute('SELECT player_name, height FROM Player')
 res = curr.fetchall()
-print(res)
 
 df = pd.DataFrame(res, columns=['Player name',  'Height'])
 print(df)
