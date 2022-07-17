626530d2f96deb3f1bb8686006c52b65062636f1
imi78 <imi78@abv.bg>
2022-07-17 21:52:26 +0300

    NumPy folder added

--- Pandas/my_script.py
+++ Pandas/my_script.py
@@ -1,12 +1,13 @@
 import pandas as pd
 import sqlite3
+import time
+start = time.time()
 
 # first connect to SQL DB
 conn = sqlite3.connect('/home/ivo/Python3/SQL/db.sqlite')
 
 # Now make a dataframe with selected SQL command and pass the connection as a second parameter of 'sql_read' function
 df = pd.read_sql('SELECT player_name, birthday FROM Player', conn)
-
-if __name__ == '__main__':
-    print(df)
+print(df)
+print(time.time() - start)
 
--- main.py
+++ main.py
@@ -1,5 +1,7 @@
 import pandas as pd
 import sqlite3
+import time
+start = time.time()
 
 conn = sqlite3.connect('/home/ivo/Python3/SQL/db.sqlite')
 curr = conn.cursor()
@@ -8,5 +10,6 @@ res = curr.fetchall()
 
 df = pd.DataFrame(res, columns=['Player name',  'Height'])
 print(df)
+print(time.time() - start)
 
 
