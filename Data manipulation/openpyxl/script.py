c6c6f3a34d08c7b605ed231a99f8975a44b38f68
imi78 <imi78@abv.bg>
2022-07-20 22:34:37 +0300

    ...

--- Data manipulation/openpyxl/script.py
+++ Data manipulation/openpyxl/script.py
@@ -2,11 +2,14 @@
 from transliterate import translit
 import openpyxl
 from googletrans import Translator
+import time
+
+start = time.time()
 # instantiate
 translator = Translator()
 
 # open and read excel file
-wb = openpyxl.load_workbook('list.xlsx')
+wb = openpyxl.load_workbook('/home/ivo/Python3/Data manipulation/openpyxl/list.xlsx')
 ws = wb.active
 
 # iterate through the specified columns
@@ -19,9 +22,11 @@ for sheet in ws.iter_rows(min_col=5, max_col=8):
         # transliterate the names
         cell.value = translit(cell.value, 'bg', reversed=True).title()
       else:
-        # translate the rest through google translate api
+        # translate the rest through google translate
         t = translator.translate(cell.value)
         cell.value = t.text
 
 # save the workbook
-wb.save('test.xlsx')
+wb.save('/home/ivo/Python3/Data manipulation/openpyxl/test.xlsx')
+
+print(time.time() - start)
