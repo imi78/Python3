388c17ade0609e2bcd65cc891d1772c7937bb9bf
imi78 <imi78@abv.bg>
2022-08-01 21:54:25 +0300

    Docker image created. Need to fix the PATH.

--- Data manipulation/openpyxl_docx/openpyxl_script.py
+++ Data manipulation/openpyxl_docx/openpyxl_script.py
@@ -1,3 +1,14 @@
+# Te program requires some modules installed before firrst use
+# googletrans module requires specific version, so we install it first
+# then install transliterate & openpyxl modules.
+# if this is the first time you are running this script, uncommment next four lines of code
+# If you are using Dockerimage file, do not do anything!
+
+# import os
+# os.system('pip install googletrans==3.1.0a0')
+# os.system('pip install transliterate')
+# os.system('pip install openpyxl')
+
 # import libraries
 from transliterate import translit
 import openpyxl
@@ -9,7 +20,7 @@ start = time.time()
 translator = Translator()
 
 # open and read excel file
-wb = openpyxl.load_workbook('/home/ivo/Python3/Data manipulation/openpyxl/list.xlsx')
+wb = openpyxl.load_workbook('/home/ivo/Python3/Data manipulation/openpyxl_docx/list.xlsx')
 ws = wb.active
 
 # iterate through the specified columns
@@ -29,7 +40,7 @@ for sheet in ws.iter_rows(min_col=5, max_col=8):
         cell.value = t.text
 
 # save the workbook
-wb.save('/home/ivo/Python3/Data manipulation/openpyxl/test.xlsx')
+wb.save('/home/ivo/Python3/Data manipulation/openpyxl_docx/result.xlsx')
 
 # Calculate the time of the code implementation
 print(time.time() - start)
