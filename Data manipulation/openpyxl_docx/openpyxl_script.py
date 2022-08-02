ee7509a42e4f1932508fefa7b42f5797f5f3f31d
imi78 <57959690+imi78@users.noreply.github.com>
2022-08-02 06:57:38 +0000

    in openpyxl_script, path for excel file needs to be corrected.

--- Data manipulation/openpyxl_docx/openpyxl_script.py
+++ Data manipulation/openpyxl_docx/openpyxl_script.py
@@ -4,10 +4,10 @@
 # if this is the first time you are running this script, uncommment next four lines of code
 # If you are using Dockerimage file, do not do anything!
 
-# import os
-# os.system('pip install googletrans==3.1.0a0')
-# os.system('pip install transliterate')
-# os.system('pip install openpyxl')
+import os
+os.system('pip install googletrans==3.1.0a0')
+os.system('pip install transliterate')
+os.system('pip install openpyxl')
 
 # import libraries
 from transliterate import translit
@@ -20,18 +20,17 @@ start = time.time()
 translator = Translator()
 
 # open and read excel file
-wb = openpyxl.load_workbook('/home/ivo/Python3/Data manipulation/openpyxl_docx/list.xlsx')
+wb = openpyxl.load_workbook('list.xlsx')
 ws = wb.active
 
 # iterate through the specified columns
 for sheet in ws.iter_rows(min_col=5, max_col=8):
   for cell in sheet:
-    # if cell.value in [None,'RANK','FIRST NAME','FAMILY NAME','POSITION']: --> shorter and faster
-    if cell.value == None or cell.value == 'RANK' or cell.value == 'FIRST NAME' or cell.value == 'FAMILY NAME' or cell.value == 'POSITION':
+    if cell.value in [None,'RANK','FIRST NAME','FAMILY NAME','POSITION']:
       continue
+        
     else:
-      # if cell.value in ['FIRST NAME','FAMILY NAME']:
-      if cell.value == 'FIRST NAME' or cell.value == 'FAMILY NAME':
+      if cell.value in ['FIRST NAME','FAMILY NAME']:
         # transliterate the names
         cell.value = translit(cell.value, 'bg', reversed=True).title()
       else:
@@ -40,7 +39,7 @@ for sheet in ws.iter_rows(min_col=5, max_col=8):
         cell.value = t.text
 
 # save the workbook
-wb.save('/home/ivo/Python3/Data manipulation/openpyxl_docx/result.xlsx')
+wb.save('result.xlsx')
 
 # Calculate the time of the code implementation
 print(time.time() - start)
--- README.md
+++ README.md
@@ -2,3 +2,4 @@
 <!-- These are exercises from SoftUni Basic and Fundamentals course. -->
 <!-- Python3 projects using different frameworks are added -->
 <!-- It includes also SQL and MongoDB queries -->
+<!-- Docker is used for Python script in "Data manipulation/openpyxl_docx/" folder and for Flask project in "Projects/Flask project" folder-->
