78135a5d2750aa82add184483273245c3b3ae6c6
Ivelin Ivanov <57959690+imi78@users.noreply.github.com>
2022-12-08 15:22:24 +0200

    Update Binance.py

--- NFT/Binance.py
+++ NFT/Binance.py
@@ -1,10 +1,35 @@
-import requests, json, time
+# Import libraries
+import json
+import requests
+from itertools import zip_longest
+# Defining Binance API URL
+url1 = "https://www.binance.com/api/v3/ticker/24hr"
+data = requests.get(url1).json()
 
-url = 'https://api.binance.com/api/v3/ticker/price?symbol='
-lst = ['SOLEUR', 'DIABUSD', 'WINGUSDT', 'FORTHUSDT']
+d = {}
 
+for i in data:
+    d[i['symbol']] = float(i['priceChangePercent'])
 
-for i in lst:
-    data = requests.get(url+i).json()
-    print(f"{data['symbol']} --> {data['price']}")
-    time.sleep(1)    
+d = sorted(d.items(), key=lambda x:x[1], reverse=True)
+x = [(k,v) for k,v in d if  v > 30]
+print("\n".join([str(i) for i in x]))
+
+print()
+
+key = 'https://api.binance.com/api/v3/ticker/price?symbol='
+
+currencies = ['RVNBUSD','LEVERBUSD', 'POLYXBUSD', 'GALBUSD', 'DIABUSD', 'SOLBUSD', 'LINKEUR']
+prices = [0.02423, 0.002077, 0.1471, 1.2838]
+j = 0
+for i,k in zip_longest(currencies,prices):
+    url = key+currencies[j]
+    data = requests.get(url).json()
+    symbol, price = data['symbol'], float(data['price']) 
+    j += 1
+    if i != None and k != None:
+        print(f"{symbol} bought - {k}")
+        print(f"price is         {price:.5f}")
+        print(f"Difference -->   {price - k:.5f}\n")
+    else:
+        print(f"{symbol} -->  {price:.3f}\n")
