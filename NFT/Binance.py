ecd6145a2b1fbbf8104f94d89ad3e2bb8ede0e60
Ivelin Ivanov <57959690+imi78@users.noreply.github.com>
2023-01-24 11:47:16 +0200

    Percentages added

--- NFT/Binance.py
+++ NFT/Binance.py
@@ -19,8 +19,8 @@ print()
 
 key = 'https://api.binance.com/api/v3/ticker/price?symbol='
 
-currencies = ['RVNBUSD','LEVERBUSD', 'POLYXBUSD', 'GALBUSD', 'TBUSD', 'DIABUSD', 'LINKEUR']
-prices = [0.02423, 0.002077, 0.1471, 1.2838, 0.0196, 0.476]
+currencies = ['RVNBUSD','LEVERBUSD', 'POLYXBUSD', 'GALBUSD', 'BURGERBUSD', 'DIABUSD', 'LINKEUR']
+prices = [0.02423, 0.00207, 0.1471, 1.28380, 0.75000, 0.47600]
 j = 0
 for i,k in zip_longest(currencies,prices):
     url = key+currencies[j]
@@ -30,12 +30,14 @@ for i,k in zip_longest(currencies,prices):
     if i != None and k != None:
         print(f"{symbol} bought - {k}")
         print(f"price is         {price:.5f}")
-        print(f"Difference -->   {price - k:.5f}\n")
+        diff = price - k
+        print(f"Difference --> {round(diff, 5)}")
+        print(f"In percentage --> {(diff/price)*100:.2f}\n")
     else:
         print(f"{symbol} -->  {price:.3f}\n")
         
-currency = ['WAMPL-USDT', 'XYO-EUR']
+currency = ['WAMPL-USDT', 'XYO-EUR', 'SHPING-EUR']
 for i in currency:
     key1 = 'https://api.pro.coinbase.com/products/'+i+'/ticker'
     data = requests.get(key1).json()
-    print(f"{i} price --> {data['price']}")
+    print(f"{i} price --> {data['price']}\n")
