7ac9ddfe8b471adcfe77f8155ea14b915fd7ff98
Ivelin Ivanov <57959690+imi78@users.noreply.github.com>
2023-03-31 10:33:21 +0300

    Update Excel_data_analysis.py

--- Data manipulation/openpyxl_docx/Excel_data_analysis.py
+++ Data manipulation/openpyxl_docx/Excel_data_analysis.py
@@ -1,102 +1,67 @@
-# import libraries
-import pandas as pd
-from openpyxl import load_workbook
-import matplotlib.pyplot as plt
+"""This program calculates the refugee's numbers from two Excel workbooks.
+The first file must be named --> 13.xlxs with the figures form 24.02 to 31.12.2022.
+The second file must be named --> 16.xlxs with the figures from 01.01.2023 onward.
+The files have to be identical in terms of number of rows (from 1 to 7).
+the 4th row must be the BXPs, 
+5th row - "citizenship",
+6th row - represents the exiting and entering PAX through the BXP column,
+7th row - "Total"
+in the 7th row ("Total") the new values will be written.
+Any charts that you want to make should take the figures from the 7th row.
+The columns may vary as it reads only the identical text written in the 4th row starting from "B" column"""
 
-# three files has to be added
-# 13.xlsx - data from 24.02-31.12.2022
-# 16.xlsx - any file file from year 2023
-# last.xlsx - 24.02-31.12.2022 (the same file like 13.xlsx, but rename it!)
-# 13.xlsx is used for a placeholder for added values form both 13.xlsx and 16.xlsx
+
+# import libraries
+from openpyxl import load_workbook
 
 #set the acive excel worksheets
-wb13 = load_workbook("13.xlsx").active
-wb16 = load_workbook("16.xlsx").active
-wb_last = load_workbook("last.xlsx").active
-# prepare the datasets
-lst13 = []
-lst16 = []
-last = []
-d = {}
-# iterate through two files and extract the cell values
-for col13, last_col in zip(wb13.iter_cols(min_row=4, min_col=2, max_row=7), wb_last.iter_cols(min_row=4, min_col=2, max_row=7)):
+wb13 = load_workbook("13.xlsx", data_only=True)
+wb16 = load_workbook("16.xlsx", data_only=True)
 
-    for cell13, cell_last in zip(col13, last_col):
-        if cell13.value == None or cell_last == None:
-            continue
-        # append the values to list
-        lst13.append(cell13.value)
-        last.append(cell_last.value)
+# set the active worksheet
+ws13 = wb13.active
+ws16 = wb16.active
 
-# extract the new values from 2023
-for col16 in wb16.iter_cols(min_row=4, min_col=2, max_row=7):
-    for cell16 in col16:
-        if cell16.value == None:
-            continue
-        # append the values to list
-        lst16.append(cell16.value)
+# function for sum up the values from both files
+def sum_values():
+    # get the column letter and change it to int ("A" > 1)
+    colnum13 = col13[0].column
+    colnum16 = col16[0].column
+    val13_exit, val16_exit = ws13.cell(row=6, column=colnum13), ws16.cell(row=6, column=colnum16)
+    val13_enter, val16_enter = ws13.cell(row=6, column=colnum13+1), ws16.cell(row=6, column=colnum16+1)
+    
+    # sum the values
+    sum_exit = val13_exit.value + val16_exit.value
+    sum_enter = val13_enter.value + val16_enter.value
+              
+    # get the cells coordinates (e.g. "B7", "C7")
+    cell_to_write_exit = ws13.cell(row=7, column=colnum13).coordinate
+    cell_to_write_enter = ws13.cell(row=7, column=colnum13+1).coordinate
+    # write the values to row No. 7
+    ws13[cell_to_write_exit] = sum_exit
+    ws13[cell_to_write_enter] = sum_enter
 
-# iterate through lists and sum the numbers
-for i in range(0, len(lst13), 5):
-    for j in range(0, len(lst16), 5):
-        bxp13 = lst13[i]
-        bxp16 = lst16[j]
-        if bxp13.startswith("ОБЩО") == False or bxp16.startswith("ОБЩО") == False:
-            if bxp13 == bxp16:
-                lst13[i+2] += lst16[j+2]
-                lst13[i+4] += lst16[j+4]
-        else:
-        # this calculates the "Total" cell values
-            lst13[i+1] += lst16[j+1]
-            lst13[i+3] += lst16[j+3]
-      
-# iterate through the new lists and calculate the difference in the numbers
-for i, j in zip(range(0, len(lst13), 5), range(0, len(last), 5)):
-    bxp = lst13[i]
-    if bxp.startswith("ОБЩО") == False:
-        # variables are made for readability
-        exit13, enter13 = lst13[i + 2], lst13[i + 4]
-        exit_last, enter_last = last[i + 2], last[i + 4]
-        exit_diff, enter_diff = (exit13 - exit_last), (enter13 - enter_last)
-
-        # put all the info in dict if greater than 3 digit number
-        if exit_diff > 100 and enter_diff > 100:
-            d[bxp[4:]] = [exit_diff,enter_diff]
-
-    else:
-        # this calculates the "Total" cell values
-        total_exit13, total_enter13 = lst13[i + 1], lst13[i + 3]
-        total_exit_last, total_enter_last = last[i + 1], last[i + 3]
+for col13 in ws13.iter_cols(min_row=4, min_col=2, max_row=4):
+    for col16 in ws16.iter_cols(min_row=4, min_col=2, max_row=4):
         
-        exit_diff, enter_diff = (total_exit13 - total_exit_last), (total_enter13 - total_enter_last)
-        d[bxp[:4]] = [exit_diff, enter_diff]
+        # skip the merged cells
+        if type(col13[0]).__name__ == 'MergedCell':
+            continue
+             
+        # checks for empty cells
+        if col13[0].value == None or col16[0].value == None:
+            continue
 
-# make the dataframe
-df = pd.DataFrame.from_dict(d, orient='index', columns=['Exiting', 'Entering'])
-#reset the index by default
-df.reset_index(inplace=True)
-# change the name of the columns, so 'BXP' is added
-df.columns = ['BXP', 'Exiting', 'Entering']
-
-# plot the data from DF with grid
-ax = df.plot(x='BXP', y=['Exiting', 'Entering'], kind='bar', grid=True)
-# sets the bar labels to be rotated for visibility
-ax.bar_label(ax.containers[0], label_type='edge', rotation=90, padding=5)
-ax.bar_label(ax.containers[1], label_type='edge', rotation=90, padding=5)
-# sets the margins of subplots (figure is visible at all corners)
-plt.subplots_adjust(left=0.05,
-                    bottom=0.374,
-                    right=0.98,
-                    top=0.95,
-                    wspace=0.4,
-                    hspace=0.4)
-
-# get current figure
-figure = plt.gcf() 
-# set the figure to be expanded, so it can be visible
-figure.set_size_inches(13, 8)
-# saves the figure as picture
-plt.savefig('foo.jpg')
-# or show it
-plt.show()
-
+        # check if the values are the same
+        if col13[0].value == col16[0].value:
+            if col13[0].value.startswith("ОБЩО"):
+                sum_values()
+                break
+            else:
+                sum_values()
+                
+    else:  # only execute when it's no break in the inner loop
+        continue
+    break # breaks the outer loop
+    
+wb13.save('test.xlsx')
