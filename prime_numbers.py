edb7399b84e646c1636803982da671d8aff91ab5
imi78 <imi78@abv.bg>
2023-10-16 21:28:33 +0300

    prime numbers in given range

--- prime_numbers.py
+++ prime_numbers.py
@@ -1,5 +1,7 @@
-def is_prime(n):
-  for i in range(2,n):
-    if (n%i) == 0:
-      return False
-  return True
+from sympy import *
+
+low = int(input('Set lower range: '))
+high = int(input('Set higher range: '))
+
+print(list(primerange(low, high)))
+      
